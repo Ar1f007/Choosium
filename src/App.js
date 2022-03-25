365f52ddd3ef2e54e3fccbd147b3c85ba19d1a20
Ar1f007 <muhammadarif9416@yahoo.com>
2022-03-26 01:16:34 +0600

    created mock data and fetched it using useEffect

--- src/App.js
+++ src/App.js
@@ -1,4 +1,15 @@
+import { useEffect, useState } from 'react';
+
 function App() {
+  const [players, setPlayers] = useState([]);
+
+  useEffect(() => {
+    fetch('mock-data.json')
+      .then((res) => res.json())
+      .then((data) => setPlayers(data))
+      .catch((err) => console.log(err));
+  }, []);
+
   return <div>App</div>;
 }
 
