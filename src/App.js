497702981cf0b8e5cb4c9fb5070ef716331e6f89
Ar1f007 <muhammadarif9416@yahoo.com>
2022-03-26 17:19:07 +0600

    sidebar stling done

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
       </header>
       <main>
         <Cards />
-        {/* <Sidebar /> */}
+        <Sidebar />
       </main>
     </>
   );
--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,4 +1,30 @@
+import './sidebar.css';
+
 const Sidebar = () => {
-  return <div>Sidebar</div>;
+  return (
+    <div className="sidebar">
+      <h2>Your Selection</h2>
+      <div className="players-list">
+        <div className="player">
+          <img
+            src="https://mundoalbiceleste.com/wp-content/uploads/2019/11/messidortmund.jpg"
+            alt="messi"
+          />
+          <h3>Zlatan Ibrahimovic</h3>
+        </div>
+        <div className="player">
+          <img
+            src="https://mundoalbiceleste.com/wp-content/uploads/2019/11/messidortmund.jpg"
+            alt="messi"
+          />
+          <h3>Robert Lewandowski</h3>
+        </div>
+      </div>
+      <div className="actions">
+        <button className="btn choose-again-btn btn-block">Choose Again</button>
+        <button className="btn select-randomly-btn btn-block">Random Select</button>
+      </div>
+    </div>
+  );
 };
 export default Sidebar;
